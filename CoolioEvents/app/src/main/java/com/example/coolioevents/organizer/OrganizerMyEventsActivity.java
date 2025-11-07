3e0f8a8f7bc55049389f4fade2e4a8764ab59c68
Ethan <eldiep@ualberta.ca>
2025-11-07 11:27:45 -0700

    added documentation

--- CoolioEvents/app/src/main/java/com/example/coolioevents/organizer/OrganizerMyEventsActivity.java
+++ CoolioEvents/app/src/main/java/com/example/coolioevents/organizer/OrganizerMyEventsActivity.java
@@ -29,7 +29,34 @@ import com.google.firebase.firestore.FirebaseFirestore;
 
 import java.util.ArrayList;
 import java.util.Collections;
-
+/**
+ * Copyright 2025 Ethan Diep & Juliane Phan & Avery Dancocks
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * PURPOSE:
+ * This class represents the Organizer's My Events Activity.
+ * It allows the organizer to look at the events they have posted
+ * and view more details about them by pressing on them.
+ *
+ * RATIONALE:
+ * Utilizes an event view model to fetch the Organizer's
+ * events and display it on a Listview.
+ *
+ * @author Ethan Diep & Juliane Phan & Avery Dancocks
+ * @version 1.0
+ * @since 2025-11-06
+ */
 public class OrganizerMyEventsActivity extends AppCompatActivity {
     FirebaseAuth mAuth;
     FirebaseUser user; // Current organizer user
