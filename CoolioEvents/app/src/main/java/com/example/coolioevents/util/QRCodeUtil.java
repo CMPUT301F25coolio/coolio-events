8dc68512e28a4aa1fff9bd0ed910328bfac2cf43
ParthMittal2017 <pmittal1@ualberta.ca>
2025-11-07 14:47:20 -0700

    Add documentation and copyright to QRCodeUtil
    Added copyright notice and class documentation for QRCodeUtil.

--- CoolioEvents/app/src/main/java/com/example/coolioevents/util/QRCodeUtil.java
+++ CoolioEvents/app/src/main/java/com/example/coolioevents/util/QRCodeUtil.java
@@ -5,6 +5,40 @@ import com.google.zxing.BarcodeFormat;
 import com.google.zxing.WriterException;
 import com.google.zxing.common.BitMatrix;
 import com.google.zxing.qrcode.QRCodeWriter;
+/**
+ * Copyright 2025 Parth Mittal
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * PURPOSE:
+ * This utility class provides static methods to generate QR codes as Bitmap images.
+ * It supports both the newer and older method names (“generate” and “make”) for
+ * compatibility with teammate code. The QR code is generated using the ZXing library.
+ *
+ * RATIONALE:
+ * Keeping QR generation logic in a separate utility class ensures reusability across
+ * different activities and services, such as event creation or sharing links.
+ * It centralizes encoding behavior and prevents duplicate code.
+ *
+ * OUTSTANDING ISSUES:
+ * The current implementation does not handle invalid or empty content strings.
+ * QR code color customization or error correction level adjustment is not yet supported.
+ * Future versions could include automatic scaling or saving to storage options.
+ *
+ * @author Parth Mittal
+ * @version 1.0
+ * @since 2025-11-07
+ */
 public class QRCodeUtil {
     //  Original method used by code
     public static Bitmap generateQRCode(String content) throws WriterException {
