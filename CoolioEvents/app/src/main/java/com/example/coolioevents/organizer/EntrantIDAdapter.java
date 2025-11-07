804fc2944e37ac0ccd02925f15768e3c019ddeb8
ParthMittal2017 <pmittal1@ualberta.ca>
2025-11-07 14:35:19 -0700

    Enhance documentation for EntrantIDAdapter class
    Added detailed documentation comments for the EntrantIDAdapter class, including copyright, purpose, rationale, and outstanding issues.

--- CoolioEvents/app/src/main/java/com/example/coolioevents/organizer/EntrantIDAdapter.java
+++ CoolioEvents/app/src/main/java/com/example/coolioevents/organizer/EntrantIDAdapter.java
@@ -6,6 +6,40 @@ import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 import java.util.ArrayList;
 import java.util.List;
+/**
+ * Copyright 2025 Parth Mittal
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * PURPOSE:
+ * This adapter provides a simple RecyclerView implementation that lists entrant
+ * IDs as plain text. It is used by the OrganizerEntrantsActivity to display
+ * the waitlist, chosen, or final entrants for a given event.
+ *
+ * RATIONALE:
+ * The adapter was kept intentionally simple for readability and maintainability.
+ * Since the entrant lists are relatively short, a minimal adapter using
+ * the default Android layout was sufficient without requiring optimizations.
+ *
+ * OUTSTANDING ISSUES:
+ * The adapter currently only supports a single TextView layout and assumes all
+ * entrant data is provided as strings. Future improvements could include
+ * adding click listeners or a more detailed layout with additional entrant info.
+ *
+ * @author Parth Mittal
+ * @version 1.0
+ * @since 2025-11-07
+ */
 /*
   Simple RecyclerView adapter that just lists entrant IDs as plain text.
   Used in OrganizerEntrantsActivity for showing waitlist/chosen/final lists
